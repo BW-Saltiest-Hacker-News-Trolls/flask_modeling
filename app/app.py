ea6276d1dfd77bb2fce4b4b496429bf925f5a530
ElisabethShah <ElisabethShah@users.noreply.github.com>
2019-12-27 17:21:23 -1000

    Add response header for JSON.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+import json
 import urllib.request
 
 app = Flask(__name__)
@@ -13,7 +14,8 @@ def comment():
                           default='10309052',
                           type=str)
     url = f'https://hacker-news.firebaseio.com/v0/item/{id}.json'
-    fp = urllib.request.urlopen(url)
-    urlbytes = fp.read()
-    fp.close()
-    return urlbytes
+    with urllib.request.urlopen(url) as response:
+        charset = response.headers.get_content_charset()
+        text = response.read().decode(charset)
+    
+    return jsonify(json.loads(text))
