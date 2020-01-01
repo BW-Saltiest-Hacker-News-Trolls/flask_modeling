043be680dd1beb573ed2db4b6199b1d32a846dc8
ElisabethShah <ElisabethShah@users.noreply.github.com>
2019-12-31 17:26:39 -1000

    Add (terrible) custom NN prediction.

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
 import json
+import numpy as np
 import urllib.request
 
 from flask import Flask, jsonify, request
+from tensorflow.keras.models import load_model
 from profanity_check import predict_prob
 
-from helper import preprocess
+from helper import preprocess, vectorize
 
 app = Flask(__name__)
+FILEPATH='simple_nn.h5'
 
 @app.route('/')
 def hello():
@@ -18,6 +21,9 @@ def item():
                           default='10309052',
                           type=str)
     url = f'https://hacker-news.firebaseio.com/v0/item/{id}.json'
+    
+    model = load_model(FILEPATH)
+    
     with urllib.request.urlopen(url) as response:
         charset = response.headers.get_content_charset()
         text = response.read().decode(charset)
@@ -25,5 +31,8 @@ def item():
     json_item = json.loads(text)
     json_item['text'] = preprocess(json_item['text'])
     json_item['prob_offensive'] = predict_prob([json_item['text']])[0]
+    print(model.predict([vectorize(json_item['text'])])[0][0])
+    json_item['custom_pred'] = np.float64(model.predict([vectorize(
+            json_item['text'])])[0][0])
     
     return jsonify(json_item)
--- app/helper.py
+++ app/helper.py
@@ -1,6 +1,10 @@
+import pandas as pd
 import re
+import spacy
+import string
 
 from html.parser import HTMLParser
+from joblib import load
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -22,3 +26,19 @@ def preprocess(text):
     text = re.sub('\s+', ' ', text)
     text = text.strip()
     return text
+
+def vectorize(text):
+    nlp = spacy.load('en_core_web_sm')
+    tfidf = load('tfidf.joblib')
+    wordlist = load('wordlist.joblib')
+    
+    text = text.lower()
+    text = str(re.sub('[{}]'.format(string.punctuation), '', text))
+    text = text.strip()
+    
+    tokens = nlp.tokenizer(text)
+    lemmas = [' '.join([token.lemma_ for token in tokens])]
+
+    vec = tfidf.transform(lemmas).todense()
+    df = pd.DataFrame(vec, columns=tfidf.get_feature_names())
+    return(df[wordlist])
